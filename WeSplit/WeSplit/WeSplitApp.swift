78e50b61da3a6cea6337b2b1205b88203729a5f8
rahul2227 <rahul2227.l115@gmail.com>
2025-01-14 23:54:36 +0100

    Folder content restructuring and done with first app learning code - day17

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Rahul Sharma on 14/01/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
