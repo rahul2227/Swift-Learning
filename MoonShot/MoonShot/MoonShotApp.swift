f1548d8524f38be77bded9265b068294f78ecb8a
rahul2227 <rahul2227.l115@gmail.com>
2025-02-14 10:29:34 +0100

    New moonshot application to experiment with Image View

---
+++ MoonShot/MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Rahul Sharma on 13/02/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
